293e9a6fe95413c1b23903e1d97fdc4c2654a30e
Birk Eidsvik <birkbjonnes@gmail.com>
2023-05-29 02:06:22 +0200

    cheeky

--- src/app/buttons.tsx
+++ src/app/buttons.tsx
@@ -3,11 +3,25 @@
 import { signIn, signOut } from "next-auth/react";
 
 export const Login = () => {
+  console.log(
+    "NODE  ENV",
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXTAUTH_URL
+      : "http://localhost:3000/create"
+  );
+
+  const callback =
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXTAUTH_URL
+      : "http://localhost:3000/create";
+
   return (
     <button
       className="py-4 px-8 bg-green-500 text-white rounded-lg"
       onClick={() =>
-        signIn("spotify", { callbackUrl: "http://localhost:3000/create" })
+        signIn("spotify", {
+          callbackUrl: callback,
+        })
       }
     >
       Sign in with spotify
